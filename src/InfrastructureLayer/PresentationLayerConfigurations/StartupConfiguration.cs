a2cff94738b21575e68486f21db764e2df481b93
M.Gholamizadeh <pranceoffire57@gmail.com>
2023-07-01 13:15:57 +0330

    add Quartz Background Job Service to project and implementing an infrastructure for it properly #faz_completely

--- src/InfrastructureLayer/PresentationLayerConfigurations/StartupConfiguration.cs
+++ src/InfrastructureLayer/PresentationLayerConfigurations/StartupConfiguration.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Quartz;
 using System.Reflection;
 
 namespace InfrastructureLayer.PresentationLayerConfigurations
@@ -30,6 +31,7 @@ namespace InfrastructureLayer.PresentationLayerConfigurations
                 .AddAuthorization()
                 .AddApiVersioning()
                 .AddEndpointsApiExplorer()
+                .AddQuartzBackgroundJob()
                 .AddHangfireBackgroundJobConfiguration(configuration)
                 .AddSwaggerGen();
         }
